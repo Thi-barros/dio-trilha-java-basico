c697903fd52d258a7879ef2b99d5b1b96a73ae1e
Thiago Barros <thiagoo.barros08@gmail.com>
2024-06-18 23:54:53 -0300

    Desáfio de controle de fluxo da DIO

---
+++ DesafioControleFluxo/src/Desafio/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package Desafio;
+
+	public class ParametrosInvalidosException extends Exception {
+	    public ParametrosInvalidosException(String mensagem) {
+	        super(mensagem);
+	    }
+	}
+
